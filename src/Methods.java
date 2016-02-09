5f286fce671f7a88353c6ff6f8317eb854db896b
binary13 <justinmneil@gmail.com>
2016-02-08 23:04:08 -0500

    Working on tutorial by Tim Buchalka

--- src/Methods.java
+++ src/Methods.java
@@ -21,6 +21,17 @@ public class Methods {
         displayHighScorePosition("Clyde", calculateHighScorePosition(500));
         displayHighScorePosition("Sally", calculateHighScorePosition(100));
         displayHighScorePosition("Ted", calculateHighScorePosition(50));
+
+        calculateScore("Justin", 1500);
+        calculateScore((2000));
+
+        System.out.println("\n");
+        calcFtInchesToCm(80, 0);
+        calcFtInchesToCm(128);
+        calcFtInchesToCm(5, 11.25);
+        calcFtInchesToCm(0, 1);
+        calcFtInchesToCm(0);
+        calcFtInchesToCm(156);
     }
 
     public static String calcScore(int score, int level, int bonus) {
@@ -37,5 +48,37 @@ public class Methods {
         if(score >= 500) return 2;
         if(score >= 100) return 3;
         else return 4;
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unknown player scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static double calcFtInchesToCm(double feet, double inches) {
+        if(feet >= 0 && inches >= 0 && inches < 12) {
+            double cm = (feet * 12 + inches) * 2.54d;
+            String result = "";
+            if(feet > 0) result += feet + " feet ";
+            if(inches > 0 || feet == 0) result += inches + " inches ";
+            result += "is " + cm + " centimeters.";
+            System.out.println(result);
+            return cm;
+        }
+        else return -1;
+    }
+
+    public static double calcFtInchesToCm(double inches) {
+        if(inches >= 0) {
+            int feet = (int)(inches / 12);
+            double remainder = inches % 12;
+            return calcFtInchesToCm(feet, remainder);
+        }
+        else return -1;
     }
 }
