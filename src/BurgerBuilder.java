68c91051987e167cfa217ea02e36fe8fc350afd3
Justin Neil <justinmneil@gmail.com>
2016-03-04 07:43:06 -0500

    Restructured BurgerBuilder to handle menus in separate classes, get input  via a separate handler class

--- src/BurgerBuilder.java
+++ src/BurgerBuilder.java
@@ -10,20 +10,22 @@ import java.util.ArrayList;
 
 class Burger {
     private String bunType;
+    private int patties;
     private ArrayList<String> toppings;
-    private boolean cheese;
 
-    public Burger(String bunType, boolean cheese) {
+    public Burger(String bunType, int patties) {
         this.bunType = bunType;
-        this.cheese = cheese;
+        this.patties = patties;
     }
 
-    public void showToppings() {
+    public String showToppings() {
+        String rString = "";
         int i = 1;
         for(String topping : toppings) {
-            System.out.println(i + topping);
+            rString += "\n" + i + topping;
             i++;
         }
+        return rString;
     }
 
     public void addTopping(String topping) {
@@ -39,9 +41,68 @@ class Burger {
     }
 }
 
+class InputGetter {
 
+    private BufferedReader br;
 
+    public InputGetter()
+    {
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+    }
 
+    public int getInt() throws IOException {
+        int i = 0;
+        String input = this.br.readLine();
+        try {
+            patties = Integer.parseInt(input);
+        } catch (NumberFormatException nfe) {
+            System.err.println("Invalid input!");
+            return -1;
+        }
+        return i;
+    }
+
+    public String getString() throws IOException {
+        return this.br.readLine();
+    }
+}
+
+class CustomBurgerMenu {
+
+    public CustomBurgerMenu() throws IOException {
+        InputGetter iGet = new InputGetter();
+        System.out.println("What type of bun would you like?");
+        String bun = iGet.getString();
+        System.out.println("How many patties would you like?");
+        int patties = iGet.getInt();
+
+        Burger myBurger = new Burger(bun, patties);
+
+        while (true) {
+            System.out.println("Building a Custom Burger!");
+            System.out.println("Your burger currently contains: " + myBurger.showToppings());
+            System.out.println("You may add up to four toppings.");
+            System.out.println("Choose from the following options.");
+            System.out.println("Would you like to:");
+            System.out.println("1. Add a topping.");
+            System.out.println("2. Remove a topping.");
+            System.out.println("3. Finish burger.");
+            System.out.println("4. Go back.");
+
+            int choice = iGet.getInt();
+
+            if (choice == -1) { continue; }
+            else {
+
+            }
+        }
+    }
+
+}
+
+class VeggieBurgerMenu {}
+
+class DeluxeComboMenu {}
 
 public class BurgerBuilder {
 
@@ -60,9 +121,9 @@ public class BurgerBuilder {
             System.out.println("3. Build a Deluxe Combo.");
             System.out.println("4. Exit.");
 
-            String input = br.readLine();
-            int i;
+            int i = 0;
 
+            String input = br.readLine();
             try {
                 i = Integer.parseInt(input);
             } catch (NumberFormatException nfe) {
@@ -72,28 +133,7 @@ public class BurgerBuilder {
 
             if (i == 1) {
 
-                Burger myBurger = new Burger();
 
-                while (true) {
-                    System.out.println("Building a Custom Burger!");
-                    System.out.println("Your burger currently contains: " + myBurger.print());
-                    System.out.println("You may add up to four toppings.");
-                    System.out.println("Choose from the following options.");
-                    System.out.println("Would you like to:");
-                    System.out.println("1. Add a topping.");
-                    System.out.println("2. Remove a topping.");
-                    System.out.println("3. Finish burger.");
-                    System.out.println("4. Go back.");
-
-                    input = br.readLine();
-
-                    try {
-                        i = Integer.parseInt(input);
-                    } catch (NumberFormatException nfe) {
-                        System.err.println("Invalid input!");
-                        continue;
-                    }
-                }
             }
 
         }
