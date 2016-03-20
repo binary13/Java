339849eb280ace538ab0e91ac2d401678fc34e6e
Justin Neil <justinmneil@gmail.com>
2016-03-19 22:55:04 -0400

    Structured MusicPlayer to use static inner classes

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -6,7 +6,38 @@ import java.util.LinkedList;
  */
 public class MusicPlayer {
 
-    class Playlist {
+    public static void main(String[] args) {
+
+        Playlist plist = new Playlist();
+
+        Album album = new Album("Rolling Along", "The Round Wheels");
+        album.addSong("Wheelie", 3.28);
+        album.addSong("Cruisin'", 2.56);
+        album.addSong("The Wheels on the Bus", 7.32);
+        album.addSong("The Long and Winding Road", 4.56);
+        plist.addAlbum(album);
+
+        album = new Album("Death and Puppies", "Condescending Irony");
+        album.addSong("The Ironic Twist of Poetic Justice, Part III", 10.23);
+        album.addSong("I don't like you at all", 4.17);
+        album.addSong("A Hipster Anthem", 4.44);
+        album.addSong("Nobody's Doing It", 3.45);
+        plist.addAlbum(album);
+
+        plist.play();
+
+
+
+
+
+
+
+
+    }
+
+
+
+    static class Playlist {
         private LinkedList<Song> songs;
 
         public Playlist() {
@@ -29,7 +60,7 @@ public class MusicPlayer {
         }
     }
 
-    class Album {
+    static class Album {
         private String title;
         private String artist;
         private ArrayList<Song> songs;
@@ -48,7 +79,7 @@ public class MusicPlayer {
         }
     }
 
-    class Song {
+    static class Song {
         private String title;
         private String artist;
         private double length;
