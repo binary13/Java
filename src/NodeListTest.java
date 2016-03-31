9090d6e0f5c7cd8724d09fe7bfd59242025db07a
Justin Neil <justinmneil@gmail.com>
2016-03-31 14:06:23 -0400

    Completed Abstract Classes Challenge

--- src/NodeListTest.java
+++ src/NodeListTest.java
@@ -7,6 +7,16 @@ public class NodeListTest {
 
         list.traverse(list.getRoot());
 
+        String cities = "Beckley Charleston Huntington Summersville Parkersburg Pool Mt_Lookout Clarksburg Morgantown";
 
+        for(String city: cities.split(" ")) {
+            list.addItem(new Node(city));
+        }
+
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("Mt_Lookout"));
+
+        list.traverse(list.getRoot());
     }
 }
